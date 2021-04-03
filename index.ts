384b1138fddf84ba5a27397822baf36699e65cc1
jiko21 <daikikojima0531@icloud.com>
2021-04-04 00:15:42 +0900

    fix routing

--- index.ts
+++ index.ts
@@ -13,8 +13,8 @@ const index = `<html>
 </html>`;
 
 
-async function getBlogData() {
-  const response = await fetch('https://raw.githubusercontent.com/jiko21/deno-deploy-sample/main/posts/2021-04-03.md', {
+async function getBlogData(date: string) {
+  const response = await fetch(`https://raw.githubusercontent.com/jiko21/deno-deploy-sample/main/posts/${data}.md`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json"
@@ -23,8 +23,8 @@ async function getBlogData() {
   return response.text();
 }
 
-async function blogPage(slug: string) {
-  const md = await getBlogData();
+async function blogPage(date: string) {
+  const md = await getBlogData(date);
   const result = flavMd.createFlavMd()
   .readMdText(md)
   .readCssText('.flav-md-h1 {color: red;}')
@@ -34,7 +34,6 @@ async function blogPage(slug: string) {
     <title>blog page</title>
   </head>
   <body>
-    <h1>Hello ${slug}</h1>
     ${result}
   </body>
 </html>`;
@@ -46,8 +45,11 @@ serve({
       "content-type": "text-html; charset=UTF-8"
     },
   }),
-  "/blog/:slug": async (request, {slug}) => {
-    const post = await blogPage(slug);
+  "/blog/:date": async (request, {date}) => {
+    if (!date.match(/^\d{4}-\d{2}-\d{2}$/)) {
+      return Response.error();
+    }
+    const post = await blogPage(date);
     return new Response(post, {headers: {
       "content-type": "text-html; charset=UTF-8"
     }})
